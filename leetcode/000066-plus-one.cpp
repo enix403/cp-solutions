da604e8a0b29d9cd5a3cea126aa593c9ce1d5e59
enix403 <fog.code000@gmail.com>
2026-03-15 17:58:50 +0500

    Add more problems

---
+++ leetcode/000066-plus-one.cpp
@@ -0,0 +1,107 @@
+// clang++ -std=c++17 -DCUSTOM_DEBUG main.cpp && ./a.out
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#ifdef CUSTOM_DEBUG
+#include "parts/leetcode-tree.h"
+#include "parts/printer.h"
+#endif
+
+// ----------------
+// start copy from here
+
+#define all(c) (c).begin(), (c).end()
+#define range2d(r, c, R, C) ((r) >= 0 && (r) < (R) && (c) >= 0 && (c) < (C))
+
+template <typename T>  //
+using max_heap = priority_queue<T>;
+template <typename T>  //
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+typedef unsigned short us;
+
+//
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+//
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+//
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+//
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+//
+typedef pair<ll, ll> pll;
+// clang-format off
+template <typename T> using v = vector<T>;
+template <typename T> using vv = vector<vector<T>>;
+template <typename K, typename V> using umap = unordered_map<K, V>;
+template <typename T> using uset = unordered_set<T>;
+// clang-format on
+
+const v<pii> DIRS = {
+    { 0,  1},
+    { 0, -1},
+    { 1,  0},
+    {-1,  0}
+};
+
+// ----------------
+
+const int INF = 1e9;
+
+class Solution
+{
+public:
+  vector<int> plusOne(vector<int>& digits)
+  {
+    int n = digits.size();
+    int c = 1;
+    for (int i = n - 1; i >= 0 && c > 0; --i) {
+      digits[i] += c;
+      c = digits[i] / 10;
+      digits[i] %= 10;
+    }
+    if (c)
+      digits.insert(digits.begin(), c);
+
+    return digits;
+  }
+};
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  Solution s;
+  return 0;
+}
